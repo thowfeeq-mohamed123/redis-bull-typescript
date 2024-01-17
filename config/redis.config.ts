ffa6c30bcc37996c848650740f039d6d9ebf878b
thowfeeq-mohamed123 <mohameddowbi6@gmail.com>
2024-01-17 17:42:45 +0530

    Tweaks added

--- config/redis.config.ts
+++ config/redis.config.ts
@@ -4,7 +4,11 @@ import Queue from "bull";
 const myQueue = (name: string): Bull.Queue<any> => {
   try {
     return new Queue(name, {
-      redis: process.env.REDIS_URL,
+      // redis: process.env.REDIS_URL,
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT as string),
+      },
     });
   } catch (err) {
     throw err;
